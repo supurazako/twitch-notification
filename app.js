e1d7f11622e5b076cab9e27841b8c85399847680
supurazako <redhot30atama@gmail.com>
2023-05-26 17:07:57 +0900

    emailsを追加

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ const refreshToken = process.env.REFRESH_TOKEN;
 const twitchClientSecret = process.env.TWITCH_CLIENT_SECRET;
 
 let refreshInterval;
+const refreshAccessToken = require('./utils/refreshAccessToken');
+const checkStreamInfo = require('./utils/streamInfo');
 // twitchのアクセストークンをリフッレシュ
 refreshInterval = setInterval(refreshAccessToken(twitchClientId, twitchClientSecret, twitchAccessToken, refreshToken, refreshInterval), 1000 * 60 * 30);
 
+// 配信情報を確認
+const checkStreamInfo = require('./utils/streamInfo');
+const sendDiscordNotification = require('./utils/notifications');
+const postTweet = require('./utils/notifications');
 
+setInterval (() => {
+    const result = checkStreamInfo(twitchUsername, twitchAccessToken, twitchClientId); // 戻り値を取得
+    if (result == true) {
+        // 通知を送る
+        sendDiscordNotification(twitchUsername, currentTitle);
+        postTweet(twitchUsername, currentTitle);
 
-
-setInterval(checkStreamInfo, 1000 * 15); // check every 15 seconds
+    }
+}, 1000 * 15);
