81dcb2665bb4adbc4c89523c5edd2321cc11f962
naoyuki <redhot30atama@gmail.com>
2024-05-31 20:18:21 +0900

    アクセストークンの取得方法を完全に自動で取得できる方法に変更（ユーザーアクセストークン方式からアプリアクセストークン方式

--- app.js
+++ app.js
@@ -6,50 +6,41 @@ import notifications from './utils/notifications';
 dotenv.config();
 
 const twitchClientId = process.env.TWITCH_CLIENT_ID;
+const twitchUserId = '605425209';
 const twitchUsername = 'oniyadayo';
-let twitchAccessToken = process.env.TWITCH_ACCESS_TOKEN;
-const refreshToken = process.env.REFRESH_TOKEN;
 const twitchClientSecret = process.env.TWITCH_CLIENT_SECRET;
 
-let refreshInterval;
+const getTwitchAccessToken = async () => {
+    try {
+        const response = await fetch('https://id.twitch.tv/oauth2/token', {
+            method: 'POST',
+            body: new URLSearchParams({
+                client_id: twitchClientId,
+                client_secret: twitchClientSecret,
+                grant_type: 'client_credentials'
+            })
+        });
 
-
-const refreshAccessToken = async () => {
-    try{
-        const refreshTokenUrl = `https://id.twitch.tv/oauth2/token?grant_type=refresh_token&refresh_token=${refreshToken}&client_id=${twitchClientId}&client_secret=${twitchClientSecret}`;
-        const response = await fetch(refreshTokenUrl, { method: 'POST' });
         const data = await response.json();
         twitchAccessToken = data.access_token;
-        console.log(`Access token refreshed: ${twitchAccessToken}`);
+        setTimeout(getTwitchAccessToken, (24 * 60 * 60 * 1000));
+        
     } catch (error) {
-        console.error('An error occurred while refreshin access token:', error);
+        console.error('Error during getiing access token:', error.message);
     }
-};
-  
-refreshInterval = setInterval(refreshAccessToken, 1000 * 60 * 30);
-
-
-// setInterval(refreshAccessToken(twitchClientId, twitchClientSecret, twitchAccessToken, refreshToken), 1000 * 60 * 30);
-// 上の場合だとエラーになる
+}
 
 // 配信情報を確認
 
-// getBroadcasterIdは非同期関数(async function)だから.thenで処理するか、awaitキーワードを使用できる関数内で実行する必要がある
-let twitchUserId;
+// twitchAccessTokenをグローバル変数として定義
+let twitchAccessToken;
 
-streamInfo.getBroadcasterId(twitchUsername, twitchAccessToken, twitchClientId)
-    .then(result => {
-        twitchUserId = result;
-        console.log(`result: ${result}`);
-    })
-    .catch(error => {
-        console.error(error);
-    });
-console.log(`twitchUserId: ${twitchUserId}`);
+// アクセストークンを取得
+getTwitchAccessToken();
 
 let isExecuted = false;
 
-async function notificationInterval() {
+const notificationInterval = async () => {
     try {
         // 戻り値を取得
         const { isTitleChanged, currentTitle } = await streamInfo.checkTitleChange(twitchUserId, twitchAccessToken, twitchClientId);
@@ -89,31 +80,3 @@ async function notificationInterval() {
 
 
 setInterval(notificationInterval, 1000 * 10);
-
-// async function notificationTest() {
-//     try {
-//         // 戻り値を取得
-//         let isFirst = false;
-//         const currentTitle = 'Test 悲報、おにや寝ぐせがひどい'
-//         if (isFirst == false) {
-//             try {
-//                 // 日時を YY-MM-DD HH:MM の形式で取得
-//                 const currentDate = new Date();
-//                 const options = { timeZone: 'Asia/Tokyo', year: '2-digit', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
-//                 const formattedDate = currentDate.toLocaleString('ja-JP', options).replace(/\//g, '-');
-//                 // console.log(`現在時刻 ${formattedDate}`);
-                
-
-//                 // 通知を送る
-//                 notifications.sendNotifications(twitchUsername, currentTitle, formattedDate);
-//                 console.log('sent notifications');
-//             } catch (error) {
-//                 console.error('An error occurred while sending notifications:', error);
-//             }
-//         }   
-//     } catch (error) {
-//         console.error('An error occurred:', error);
-//     }
-// }
-
-// notificationTest();
