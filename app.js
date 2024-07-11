300a538b0ed285ce4bf15e93f84e68319d6542ac
supurazako <redhot30atama@gmail.com>
2024-07-12 00:43:15 +0900

    アクセストークンのspreadsheetからの取得。およびgitignoreの更新

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import fetch from 'node-fetch';
 import streamInfo from './utils/streamInfo.js';
 import notifications from './utils/notifications.js';
+import { getSpreadsheetData } from './utils/spreadsheet.js';
 
 dotenv.config();
 
@@ -65,11 +66,33 @@ const notificationInterval = async () => {
 
 const testNotification = async () => {
     try {
-        notifications.testSendTitleChangeNotifications(twitchUsername, 'test title');
+        // notifications.testSendTitleChangeNotifications(twitchUsername, 'test title');
+        await getTwitchAccessTokenFromSpreadsheet();
+        process.exit(0);
     } catch (error) {
         console.error('An error occurred while sending notifications:', error);
     }
 }
 
-testNotification();
 // setInterval(notificationInterval, 1000 * 10);
+
+// 新しい実行フロー
+// 1. アクセストークンをGSから取得
+// 2. 通知の条件分岐を行う
+
+// アクセストークンを取得
+const getTwitchAccessTokenFromSpreadsheet = async () => {
+    // スプレッドシートからアクセストークンを取得
+    // 格納場所は暫定のため、更新が必要 :TODO
+    const spreadsheetRange = 'tokens!A2:B2';
+    const response = await getSpreadsheetData(spreadsheetRange);
+    console.log(`response: ${response}`);
+
+    // 取得したデータを格納
+    const accessToken = response[0];
+    const expiresIn = response[1];
+    console.log(`accessToken: ${accessToken}, expiresIn: ${expiresIn}`)
+
+}
+
+testNotification();
