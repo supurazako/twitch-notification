48ec142ab7d62467348b220c5e52bc58787a28c4
supurazako <redhot30atama@gmail.com>
2024-07-24 15:36:43 +0900

    change: 通知の送信システムを変更し、タイトルと開始通知で分ける

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import { checkTitleChange, checkStreamStatusChange } from './utils/streamInfo.js';
-import notifications from './utils/notifications.js';
+import { testSendStreamStartNotifications, testSendTitleChangeNotifications } from './utils/notifications.js';
 import { getTwitchAccessTokenFromSpreadsheet } from './utils/tokens.js';
 
 dotenv.config();
@@ -10,31 +10,6 @@ const twitchUserId = '605425209';
 const twitchUsername = 'oniyadayo';
 
 // TODO: 配信が開始されたら通知を送る
-
-const notificationInterval = async () => {
-    try {
-        // 配信状態を確認
-        const isStreamStarted = await streamInfo.checkStreamStatusChange(twitchUserId, twitchAccessToken, twitchClientId);
-        if (isStreamStarted == true) {
-            // 配信が開始されたら通知を送る
-            notifications.sendNotifications(twitchUsername, '配信が開始されました');
-            console.log('sent notifications');
-        }
-        // タイトルの変更を確認
-        const { isTitleChanged, currentTitle } = await streamInfo.checkTitleChange(twitchUserId, twitchAccessToken, twitchClientId);
-        if (isTitleChanged == true) {
-            try {
-                // 通知を送る
-                notifications.sendNotifications(twitchUsername, currentTitle);
-                console.log('sent notifications');
-            } catch (error) {
-                console.error('An error occurred while sending notifications:', error);
-            }
-        }
-    } catch (error) {
-        console.error('An error occurred:', error);
-    }
-}
 
 const testMain = async () => {
     try {
@@ -47,9 +22,8 @@ const testMain = async () => {
         console.log(`isStreamStarted: ${isStreamStarted}`);
         // trueならば、配信が開始されたことを通知
         if (isStreamStarted) {
-            // TODO: 名前つきインポート及びエクスポートに変更
-            // notifications.sendNotifications(twitchUsername);
-            console.log('sent notifications');
+            await testSendStreamStartNotifications(twitchUsername);
+            console.log('sent start notifications');
         }
 
         // タイトルの変更を確認
@@ -57,9 +31,8 @@ const testMain = async () => {
         console.log(`isTitleChanged: ${isTitleChanged}`);
         // trueならば、タイトルが変更されたことを通知
         if (isTitleChanged) {
-            // TODO: 名前つきインポート及びエクスポートに変更
-            // notifications.sendNotifications(twitchUsername, currentTitle);
-            console.log('sent notifications');
+            await testSendTitleChangeNotifications(twitchUsername, currentTitle);
+            console.log('sent title change notifications');
         }
 
         // Lambda用の退出処理
@@ -73,10 +46,10 @@ const testMain = async () => {
 
 // 新しい実行フロー
 // 1. アクセストークンをGSから取得 ←CLEAR！
-// 2. 配信状態を確認
-// 3. 配信が開始されたら通知を送る
-// 4. タイトルの変更を確認
-// 5. タイトルが変更されたら通知を送る
+// 2. 配信状態を確認　←CLEAR！
+// 3. 配信が開始されたら通知を送る　←CLEAR！
+// 4. タイトルの変更を確認　←CLEAR！
+// 5. タイトルが変更されたら通知を送る　←CLEAR！
 // 6. AWS Lambdaに実行できるようにする
 
 testMain();
